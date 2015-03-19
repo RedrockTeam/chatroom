60d4a3b16fe8f572032a82cae9074e117188f6a6
liuhzz <847858699@qq.com>
2015-03-19 22:08:20 +0800

    debugger

--- js/date.js
+++ js/date.js
@@ -3,7 +3,7 @@
             var  begin = new Date();
             var submit = document.querySelector('#join_img');
             var body = document.body;
-            var tips = document.querySelector("#tips");
+
             begin.setFullYear(2015,3,18);
             var  end = new Date();
             end.setFullYear(2015,3,20);
@@ -13,9 +13,7 @@
             	join_img.style.background = 'gray';
                 join_img.onclick = null;
                 body.style.color = 'gray';
-                tips.style.display = 'block';
-                tips.addEventListener('click',function(){
-                	this.style.display = 'none';
-                },false)
+            
+               
             }
         }
